56281f75c460f16b636feaaf4c7edba30c5adbb5
Arkadiusz Skalski <askalski85@gmail.com>
2019-10-28 15:33:09 +0100

    chore: no sudo for docker build command
    You should not use sudo in order to interact with docker
    command. Instead you should add your user to docker group,
    details here: https://docs.docker.com/v17.12/install/linux/linux-postinstall/
    
    Signed-off-by: Arkadiusz Skalski <askalski85@gmail.com>

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ JMETER_VERSION="5.1.1"
 
 # Example build line
 # --build-arg IMAGE_TIMEZONE="Europe/Amsterdam"
-sudo docker build  --build-arg JMETER_VERSION=${JMETER_VERSION} -t "justb4/jmeter:${JMETER_VERSION}" .
+docker build  --build-arg JMETER_VERSION=${JMETER_VERSION} -t "justb4/jmeter:${JMETER_VERSION}" .
