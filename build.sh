36b0f17098754d3dd56779e1ce78da3dea3da9c6
David Paz <david.paz@lbbw.de>
2021-05-11 15:41:32 +0200

    Added env variable to match README file
    Updated README file to include environment variables for version and
    timezone.

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 JMETER_VERSION=${JMETER_VERSION:-"5.4"}
+IMAGE_TIMEZONE=${IMAGE_TIMEZONE:-"Europe/Amsterdam"}
 
 # Example build line
-docker build  --build-arg JMETER_VERSION=${JMETER_VERSION} --build-arg TZ="Europe/Berlin" -t "justb4/jmeter:${JMETER_VERSION}" .
+docker build  --build-arg JMETER_VERSION=${JMETER_VERSION} --build-arg TZ=${IMAGE_TIMEZONE} -t "justb4/jmeter:${JMETER_VERSION}" .
