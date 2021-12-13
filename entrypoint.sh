b463ebb3b2c8085e8a6510ce6eed13f77a148243
Just van den Broecke <just@justobjects.nl>
2021-12-13 17:02:25 +0100

    #47 make patch double sure that -D arg is passed

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,14 +19,17 @@ freeMem=`awk '/MemFree/ { print int($2/1024) }' /proc/meminfo`
 s=$(($freeMem/10*8))
 x=$(($freeMem/10*8))
 n=$(($freeMem/10*2))
-export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m -Dlog4j2.formatMsgNoLookups=true"
+export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m"
 
 echo "START Running Jmeter on `date`"
 echo "JVM_ARGS=${JVM_ARGS}"
 echo "jmeter args=$@"
 
 # Keep entrypoint simple: we must pass the standard JMeter arguments
-jmeter $@
+EXTRA_ARGS=-Dlog4j2.formatMsgNoLookups=true
+echo "jmeter ALL ARGS=${EXTRA_ARGS} $@"
+jmeter ${EXTRA_ARGS} $@
+
 echo "END Running Jmeter on `date`"
 
 #     -n \
