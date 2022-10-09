2fb89751265966c1d8c5795905da06c029db9913
Archimedes Trajano <developer@trajano.net>
2022-10-08 23:29:23 -0400

    compute using cgroup
    Fixes #68

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@ fi
 
 # Execute JMeter command
 set -e
-freeMem=`awk '/MemAvailable/ { print int($2/1024) }' /proc/meminfo`
+freeMem=$(echo $(cat  /sys/fs/cgroup/memory/memory.limit_in_bytes ) / 1024 / 1024 | bc)
 
 [[ -z ${JVM_XMN} ]] && JVM_XMN=$(($freeMem/10*2))
 [[ -z ${JVM_XMS} ]] && JVM_XMS=$(($freeMem/10*8))
